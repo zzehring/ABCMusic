8efde3e8227572e9c8e2787347fb3998faa71f6b
Zachary Zehring <zzehring@gmail.com>
2015-12-07 18:35:40 -0500

    Update DB

---
+++ resources/views/albums/index.blade.php
@@ -0,0 +1,37 @@
+@extends('template')
+
+@section('content')
+    <h1>ABC Music Instruments</h1>
+    <hr>
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+        <tr class="bg-info">
+            <th>Album ID</th>
+            <th>Title</th>
+            <th>Copyright Date</th>
+            <th>Format</th>
+            <th>Producer ID</th>
+            <th colspan="3">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($albums as $album)
+            <tr>
+                <td>{{ $album->id }}</td>
+                <td>{{ $album->title }}</td>
+                <td>{{ $album->copyright_date }}</td>
+                <td>{{ $album->format }}</td>
+                <td>{{ $album->producer_id }}</td>
+                {{--<td><a href="{{route('musician.edit',$musician->id)}}" class="btn btn-warning">Update</a></td>--}}
+                {{--<td>--}}
+                {{--{!! Form::open(['method' => 'DELETE', 'route'=>['songs.destroy', $song->song_id]]) !!}--}}
+                {{--{!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}--}}
+                {{--{!! Form::close() !!}--}}
+                {{--</td>--}}
+            </tr>
+        @endforeach
+
+        </tbody>
+
+    </table>
+@endsection
