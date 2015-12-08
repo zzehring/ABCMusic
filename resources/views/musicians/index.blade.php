9191cda6d94bac1627f885490b1edbaecbac4b10
Zachary Zehring <zzehring@gmail.com>
2015-12-08 14:00:39 -0500

    Listing albums from artist

--- resources/views/musicians/index.blade.php
+++ resources/views/musicians/index.blade.php
@@ -27,7 +27,7 @@
                     {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
                     {!! Form::close() !!}
                 </td>
-                <td><a href="{{route('musicians.show',$musician->id)}}" class="btn btn-warning">Show Albums</a></td>
+                <td><a href="{{route('musicians.albums_by_musician',$musician->id)}}" class="btn btn-success">Show Albums</a></td>
             </tr>
         @endforeach
 
