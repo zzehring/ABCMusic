332b7f96df4ef058bfea822ade31997ab1ac3e93
Zachary Zehring <zzehring@gmail.com>
2015-12-08 15:45:03 -0500

    listing albums in 2005

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>ABC Music Albums</h1>
     {{--<a href="{{route('albums.album_date')}}" class="btn btn-success">How Many Albums from 2005?</a>--}}
-    <h2>{{ $count->albumsInYear }} Albums from 2005</h2>
+    <h2>{{ $count }} Albums from 2005</h2>
     <hr>
     <table class="table table-striped table-bordered table-hover">
         <thead>
